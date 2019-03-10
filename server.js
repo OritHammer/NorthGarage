a81f25eef0ccb36cae060be981a92a545aad29d9
leeorrH <leeorrh@gmail.com>
2019-03-10 13:02:08 +0200

    registar+login work

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
     /********** DB Zone *************/ 
     var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://localhost:27017/projectDB";
+    var url = "mongodb://localhost:27017/";
     var express =require('express');
     var app = express();
     app.use(express.json()); 
@@ -11,78 +11,78 @@
     app.use('/static',express.static(path.join(__dirname,'client')));
 
     var server=app.listen(3000, ()=> {console.log("server in ...")});
-    success=false;
+    // success=false;
 
-  /** mongo connect **/
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-        if (err) throw err;
-        var dataBaseObject = db.db("projDB");
+//   /** mongo connect **/
+//     MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+//         if (err) throw err;
+//         var dataBaseObject = db.db("projDB");
 
-//create collections
-        // adding employees collection - table
-        dataBaseObject.createCollection("employees",function(err,res){
-          console.log("employees collection created");
-            //db.close();
-        });  
+// //create collections
+//         // adding employees collection - table
+//         dataBaseObject.createCollection("employees",function(err,res){
+//           console.log("employees collection created");
+//             //db.close();
+//         });  
 
-        // adding 'works collection - table
-        dataBaseObject.createCollection("works",function(err,res){
-          console.log("works collection created!");
-           // db.close();
-        });    
+//         // adding 'works collection - table
+//         dataBaseObject.createCollection("works",function(err,res){
+//           console.log("works collection created!");
+//            // db.close();
+//         });    
 
-        // adding customers collection - table
-        dataBaseObject.createCollection("customers",function(err,res){
-          console.log("customers collection created!");
-          //  db.close();
-        }); 
-/*
-//adding works to works collection        
-var work1 = {
-  _id: "000001",
-  WorkerNumber: "965",
-  WorkerName: "Mark",
-  ClientID: "313124823",
-  CarID: "95-896-78",
- CerType:"Toyota",
-  Status:  "Pending",
-  problemDiscription: "Driver Seat Recline Position Restraints Sensor",
-  TotalCost: "450$"
- }; 
+//         // adding customers collection - table
+//         dataBaseObject.createCollection("customers",function(err,res){
+//           console.log("customers collection created!");
+//           //  db.close();
+//         }); 
+// /*
+// //adding works to works collection        
+// var work1 = {
+//   _id: "000001",
+//   WorkerNumber: "965",
+//   WorkerName: "Mark",
+//   ClientID: "313124823",
+//   CarID: "95-896-78",
+//  CerType:"Toyota",
+//   Status:  "Pending",
+//   problemDiscription: "Driver Seat Recline Position Restraints Sensor",
+//   TotalCost: "450$"
+//  }; 
 
-    dataBaseObject.collection("works").insertOne(work1);
+//     dataBaseObject.collection("works").insertOne(work1);
 
-    var work2={
-      _id: '000002',
-      WorkerNumber: "653",
-      WorkerName: 'Jacob',
-      ClientID: "313124823",
-      CarID: '95-896-78',
-      CerType:'Toyota',
-      Status: 'paid up',
-      problemDiscription: 'Throttle Actuator Control Lamp Control Circuit',
-      TotalCost:"600$"
-     }; 
-        dataBaseObject.collection("works").insertOne(work2);
-    var work3={
-      _id:'000003',
-      WorkerNumber: "653",
-      WorkerName: 'Jacob',
-      ClientID: "286532541",
-      CarID: '35-625-88',
-      CerType:'Nisan',
-      Status: 'paid up',
-      problemDiscription: 'Throttle Actuator Control Lamp Control Circuit',
-      TotalCost:"600$"
-     }
+//     var work2={
+//       _id: '000002',
+//       WorkerNumber: "653",
+//       WorkerName: 'Jacob',
+//       ClientID: "313124823",
+//       CarID: '95-896-78',
+//       CerType:'Toyota',
+//       Status: 'paid up',
+//       problemDiscription: 'Throttle Actuator Control Lamp Control Circuit',
+//       TotalCost:"600$"
+//      }; 
+//         dataBaseObject.collection("works").insertOne(work2);
+//     var work3={
+//       _id:'000003',
+//       WorkerNumber: "653",
+//       WorkerName: 'Jacob',
+//       ClientID: "286532541",
+//       CarID: '35-625-88',
+//       CerType:'Nisan',
+//       Status: 'paid up',
+//       problemDiscription: 'Throttle Actuator Control Lamp Control Circuit',
+//       TotalCost:"600$"
+//      }
   
-     dataBaseObject.collection("works").insertOne(work3);
+//      dataBaseObject.collection("works").insertOne(work3);
 
-//adding employees to employees collection
-dataBaseObject.collection("employees").insertOne({_id: "653", WorkerName: 'Jacob', password: "12345" , mail: 'Jacob@gmail.com', phone:'0523459785' });
-dataBaseObject.collection("employees").insertOne({_id: "965", WorkerName: 'Mark', password: "12345",mail: 'Mark@gmail.com', phone:'0523446885' });
-*/
-});
+// //adding employees to employees collection
+// dataBaseObject.collection("employees").insertOne({_id: "653", WorkerName: 'Jacob', password: "12345"  });
+// dataBaseObject.collection("employees").insertOne({_id: "965", WorkerName: 'Mark', password: "12345"});
+// */
+// });
 
  
 
@@ -95,67 +95,8 @@ dataBaseObject.collection("employees").insertOne({_id: "965", WorkerName: 'Mark'
  * handle request from newlogin page 
  */
 app.post('/login' , function(req,res){
-
-  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-      if (err) throw err;
-      var dataBaseObject = db.db("projDB");
-       success=false;
-    dataBaseObject.collection('employees').findOne({ WorkerName: req.body.WorkerName},
-       function(err, user) {
-              if(user ===null){
-                res.end("Login invalid");
-             }else if (user.WorkerName == req.body.WorkerName && user.password == req.body.password){
-              var worker = user ; 
-              console.log("user found");
-              success=true;
-              // res.render('completeprofile',{profileData:user});
-           }else{
-             console.log("Credentials wrong");
-             res.end("Login invalid");
-           }      
-    });
-db.close();
-  });
-if (success){
-  res.send({'success': true, 'message': 'login'});
-  success=false;
-} 
+  employeeLogin(req.body.WorkerName,req.body.password, res);
 });
-
-/** signUp
-     * by: leeorr h
-     * use : signUp if user not exist
-     *  */ 
-
-app.post('/signUp' , function(req,res){
-
-  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-      if (err) throw err;
-      var dataBaseObject = db.db("projDB");
-       success=false;
-    dataBaseObject.collection('employees').findOne({ _id: req.body.workerNum},
-       function(err, user) {
-              if(user ===null){
-                dataBaseObject.collection("employees").insertOne({_id: req.body.workerNum,
-                   WorkerName: req.body.workerName, password: req.body.password});
-               console.log("user added");
-              //need to send somthing  
-              // res.send({'success': true, 'message': 'singUp'});
-             }else {
-              console.log("user exist");
-              //need to send somthing
-              // res.send({'success': false, 'message': 'singUp'});
-           }     
-    });
-db.close();
-  });
-if (success){
-
-  success=false;
-} 
-});
-
-
 
 /** searchEmployee
      * by: leeorr h
@@ -172,15 +113,68 @@ if (success){
       }
      }
 
+/** signUp
+     * by: leeorr h
+     * use : signUp if user not exist
+     *  */ 
+app.post('/signUp' , async function(req,res){
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    var dataBaseObject = db.db("projDB");
+       success=false;
+     dataBaseObject.collection('employees').findOne({ _id: req.body.workerNum},
+       function(err, user) {
+             if(user ===null){
+                dataBaseObject.collection("employees").insertOne({_id:req.body.workerNum, 
+                    WorkerName: req.body.workerName,
+                    password: req.body.password});
+                console.log("user added"); 
+                db.close();
+                callback(true, res,user,'signUp');
+              //need to send somthing  
+              // res.send({'success': true, 'message': 'singUp'});
+             }else {
+              console.log("user exist");
+              db.close();
+              //need to send somthing
+              // res.send({'success': false, 'message': 'singUp'});
+           }     
+    });
+  });
+  
+});
+
    /** employeeLogin
      * by: leeorr h
      * use : check empId and Password, if there is a match we'll continue
      *  */ 
-    function employeeLogin(workerNameIn,passwordIn){
-      
+    function employeeLogin(workerNameIn, passwordIn, res){
+      MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dataBaseObject = db.db("projDB");
+        dataBaseObject.collection('employees').findOne({WorkerName: workerNameIn},
+          function(err, result) {
+            if (err) throw err;
+            if(result === null){
+              callback(false, res,null,'login')
+            }else if (result.WorkerName == workerNameIn && result.password == passwordIn){
+              callback(true, res,result.WorkerName,'login')
+            }
+            else{
+              callback(false, res,null,'login')
+            }
+            db.close(); 
+          });
+      });
     }
 
-
+    function callback(bool, res,obj,message){
+      var found = {'success': bool,
+                   'message': message,
+                   'obj': obj
+                  };
+      res.send(found);
+    }
 
     /** insertNewEmployee
      * by: leeorr h
@@ -221,7 +215,6 @@ if (success){
      }
 
 
-
  /**open windows**/
  /**to use this run the server and write localhost3000/.... */
  app.get('/',(req,res)=>{
@@ -249,7 +242,3 @@ collection.find({},{},function(e,docs){
 res.json(docs);
 });
 });
-
-
-
-
