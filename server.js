229b915d09cfff69096de8ecfec11c0f15a19b25
leeorrH <leeorrh@gmail.com>
2019-03-10 14:28:40 +0200

    login and sign up work!

--- server.js
+++ server.js
@@ -95,29 +95,40 @@
  * handle request from newlogin page 
  */
 app.post('/login' , function(req,res){
-  employeeLogin(req.body.WorkerName,req.body.password, res);
+  employeeLogin(req.body.WorkerNum,req.body.password, res);
 });
 
-/** searchEmployee
+ /** employeeLogin
      * by: leeorr h
-     * use : looking for employee on "employees" colloection(table) by empID
+     * use : check empId and Password, if there is a match we'll continue
      *  */ 
-    function searchEmployee(empIDIn){
-      if( dataBaseObject.employees.find({empID : { $eq:empIDIn}})){
-       console.log("user exist"); 
-       return true;
-      }
-      else {
-       console.log("user not exist"); 
-       return false;
-      }
-     }
+    function employeeLogin(workerNumIn, passwordIn, res){
+      MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dataBaseObject = db.db("projDB");
+        dataBaseObject.collection('employees').findOne({_id: workerNumIn},
+          function(err, result) {
+            if (err) throw err;
+            if(result === null){
+              callback(false, res,null,'login')
+            }else if (result._id == workerNumIn && result.password == passwordIn){
+              callback(true, res,result.WorkerName,'login')
+            }
+            else{
+              callback(false, res,null,'login')
+            }
+            db.close(); 
+          });
+      });
+    }
+
+
 
 /** signUp
      * by: leeorr h
      * use : signUp if user not exist
      *  */ 
-app.post('/signUp' , async function(req,res){
+app.post('/signUp' , function(req,res){
   MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
     if (err) throw err;
     var dataBaseObject = db.db("projDB");
@@ -130,43 +141,26 @@ app.post('/signUp' , async function(req,res){
                     password: req.body.password});
                 console.log("user added"); 
                 db.close();
-                callback(true, res,user,'signUp');
-              //need to send somthing  
-              // res.send({'success': true, 'message': 'singUp'});
+                var worker = {
+                  'workerNumber' : req.body.workerNum, 
+                  'WorkerName' : req.body.workerName,
+                  'password' : req.body.password
+                };
+                callback(true, res,worker,'signUp');
              }else {
               console.log("user exist");
+              callback(false, res,user,'signUp');
               db.close();
-              //need to send somthing
-              // res.send({'success': false, 'message': 'singUp'});
            }     
     });
   });
   
 });
 
-   /** employeeLogin
-     * by: leeorr h
-     * use : check empId and Password, if there is a match we'll continue
-     *  */ 
-    function employeeLogin(workerNameIn, passwordIn, res){
-      MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-        if (err) throw err;
-        var dataBaseObject = db.db("projDB");
-        dataBaseObject.collection('employees').findOne({WorkerName: workerNameIn},
-          function(err, result) {
-            if (err) throw err;
-            if(result === null){
-              callback(false, res,null,'login')
-            }else if (result.WorkerName == workerNameIn && result.password == passwordIn){
-              callback(true, res,result.WorkerName,'login')
-            }
-            else{
-              callback(false, res,null,'login')
-            }
-            db.close(); 
-          });
-      });
-    }
+
+
+  
+  
 
     function callback(bool, res,obj,message){
       var found = {'success': bool,
@@ -176,43 +170,7 @@ app.post('/signUp' , async function(req,res){
       res.send(found);
     }
 
-    /** insertNewEmployee
-     * by: leeorr h
-     * use : insert new employee to "employees" collection(table) by his all data
-     *  */ 
-    function insertNewEmployee(empIDIn,userNameIn,passwordIn,mailIn,phoneIn){
-      if (!searchEmployee(empIDIn)){
-      var newEmployeeData = {
-                             empID:empIDIn,
-                             userName:userNameIn ,//employee name
-                             password:passwordIn,
-                             mail:mailIn,
-                             phone:phoneIn};
-      //the fuction in the insertOne function is virtual/anonumys fuction
-      dataBaseObject.collection("employees").insertOne(newEmployeeData,function(err,res){
-        if(err) throw err;
-        console.log("new employee added successfully"+newEmployeeData)
-      });
-    }
-    else 
-    console.log("user already exist"); 
-    }
-
-    /** removeEmployee
-     * by: leeorr h
-     * use : removing spesific employee by his empID
-     *  */ 
-
-    function removeEmployee(empIDIn){
-      if(searchEmployee(empIDIn)){
-        var empToDel = {empID:empIDIn};
-        dataBaseObject.collection("employees").deleteOne(empToDel,function(err,obj){
-          console.log(obj.result.n + " document(s) deleted"); 
-        });
-      }
-      else 
-      console.log("employee not exist"); 
-     }
+  
 
 
  /**open windows**/
