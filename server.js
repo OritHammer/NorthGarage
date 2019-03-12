2f05a1e6ab4c6113481267134b2b8801cc9f786d
leeorrH <leeorrh@gmail.com>
2019-03-12 22:47:19 +0200

    add work working

--- server.js
+++ server.js
@@ -11,83 +11,10 @@
     app.use('/static',express.static(path.join(__dirname,'client')));
 
     var server=app.listen(3000, ()=> {console.log("server in ...")});
-    var workNumber = "000000" ;
-    // success=false;
 
-//   /** mongo connect **/
-//     MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-//         if (err) throw err;
-//         var dataBaseObject = db.db("projDB");
-
-// //create collections
-//         // adding employees collection - table
-//         dataBaseObject.createCollection("employees",function(err,res){
-//           console.log("employees collection created");
-//             //db.close();
-//         });  
-
-//         // adding 'works collection - table
-//         dataBaseObject.createCollection("works",function(err,res){
-//           console.log("works collection created!");
-//            // db.close();
-//         });    
-
-//         // adding customers collection - table
-//         dataBaseObject.createCollection("customers",function(err,res){
-//           console.log("customers collection created!");
-//           //  db.close();
-//         }); 
-// /*
-// //adding works to works collection        
-// var work1 = {
-//   _id: "000001",
-//   WorkerNumber: "965",
-//   WorkerName: "Mark",
-//   ClientID: "313124823",
-//   CarID: "95-896-78",
-//  CerType:"Toyota",
-//   Status:  "Pending",
-//   problemDiscription: "Driver Seat Recline Position Restraints Sensor",
-//   TotalCost: "450$"
-//  }; 
-
-//     dataBaseObject.collection("works").insertOne(work1);
-
-//     var work2={
-//       _id: '000002',
-//       WorkerNumber: "653",
-//       WorkerName: 'Jacob',
-//       ClientID: "313124823",
-//       CarID: '95-896-78',
-//       CerType:'Toyota',
-//       Status: 'paid up',
-//       problemDiscription: 'Throttle Actuator Control Lamp Control Circuit',
-//       TotalCost:"600$"
-//      }; 
-//         dataBaseObject.collection("works").insertOne(work2);
-//     var work3={
-//       _id:'000003',
-//       WorkerNumber: "653",
-//       WorkerName: 'Jacob',
-//       ClientID: "286532541",
-//       CarID: '35-625-88',
-//       CerType:'Nisan',
-//       Status: 'paid up',
-//       problemDiscription: 'Throttle Actuator Control Lamp Control Circuit',
-//       TotalCost:"600$"
-//      }
-  
-//      dataBaseObject.collection("works").insertOne(work3);
-
-// //adding employees to employees collection
-// dataBaseObject.collection("employees").insertOne({_id: "653", WorkerName: 'Jacob', password: "12345"  });
-// dataBaseObject.collection("employees").insertOne({_id: "965", WorkerName: 'Mark', password: "12345"});
-// */
-// });
-
- 
-
- 
+    /** global veriables */
+    
+    var employesDetails; 
 
 /**
  * 
@@ -111,12 +38,13 @@ app.post('/login' , function(req,res){
           function(err, result) {
             if (err) throw err;
             if(result === null){
-              callback(false, res,null,'login')
+              callback(false, res,null,'login');
             }else if (result._id == workerNumIn && result.password == passwordIn){
-              callback(true, res,result.WorkerName,'login')
+              employesDetails = result;
+              callback(true, res,result.WorkerName,'login');
             }
             else{
-              callback(false, res,null,'login')
+              callback(false, res,null,'login');
             }
             db.close(); 
           });
@@ -205,15 +133,16 @@ app.post('/addWork' , function(req,res){
     if (err) throw err;
     var dataBaseObject = db.db("projDB");
     success=false;
-    dataBaseObject.collection("employees").insertOne({_id:workNumber+1, 
+    dataBaseObject.collection("works").insertOne({_id:req.body._id, 
     WorkerNumber: req.body.workerNumber, WorkerName: req.body.workerName,
     ClientID: req.body.clientID, CarID: req.body.carID ,CarType : req.body.carType,
-    Status : req.body.status , problemDiscription: req.body.problemDiscription , TotalCost: TotalCost
+    Status : req.body.status , problemDiscription: req.body.problemDiscription , 
+    TotalCost: req.body.TotalCost
      });
     console.log("work added"); 
     db.close();
     var workDitails = {
-      'workId':workNumber, 
+      '_id':req.body._id, 
       'WorkerNumber': req.body.workerNumber,
       'WorkerName': req.body.workerName,
       'ClientID': req.body.clientID,
@@ -221,8 +150,8 @@ app.post('/addWork' , function(req,res){
       'CarType' : req.body.carType,
       'Status': req.body.status ,
       'problemDiscription': req.body.problemDiscription ,
-      'TotalCost': TotalCost
-       };
+      'TotalCost': req.body.TotalCost
+    };
      callback(true, res,workDitails,'addWork');          
   });
 });
@@ -241,6 +170,9 @@ app.post('/showWorkDetails' , function(req,res){
   });
 });
 
+app.post('/getEmpDetails' , function(req,res){
+  callback(true,res,employesDetails,'getEmpDetails');
+});
 
 
   
